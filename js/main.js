a725c9c779cc0083c31f309f0384fe5b3628a7a5
Natalie Loh <Natalie@Natalies-MacBook-Pro-2.local>
2015-05-05 14:47:17 +0100

    removed duplicate geocoder and place autocomplete search/ marker creation. just using autocomplete places api now. renamed new markers/ coordinates to  mainMarker and mainCoorinates

--- js/main.js
+++ js/main.js
@@ -1,76 +1,95 @@
 $(document).ready(function(){
+  // Declaring variables
   var map;
-  var markers = [];
-  var northEastCoord;
-  var southWestCoord;
+  var searchMarkers = [];
+  var mainMarker;
+  var mainCoordinates = {};
+
   // Sets All Markers in Array on Map
-  function setAllMap(map) {
-    console.log('set map')
-    for (var i = 0; i < markers.length; i++) {
-      markers[i].setMap(map);
-    }
-  }
+  // function setAllMap(map) {
+  //   console.log('set map')
+  //   for (var i = 0; i < markers.length; i++) {
+  //     markers[i].setMap(map);
+  //   }
+  // }
 
   // Show Markers 
-  function showMarkers() {
-    console.log('markers shown');
-    setAllMap(map);
-  }
+  // function showMarkers() {
+  //   console.log('markers shown');
+  //   setAllMap(map);
+  // }
   // Clear Markers
-  function clearMarkers(){
-    console.log('markers cleared')
-    setAllMap(null);
-  }
-  var getBusStops = function(locLat, locLong){
-    console.log(locLat);
-    console.log(locLong);
-    northEastCoord = String(locLat + 0.005) + ',' + String(locLong + 0.005)
-    southWestCoord = String(locLat - 0.005) + ',' + String(locLong - 0.005)
-    console.log(northEastCoord)
-    console.log(southWestCoord)
-  }
+  // function clearMarkers(){
+  //   console.log('markers cleared')
+  //   setAllMap(null);
+  // }
+  // var getBusStops = function(locLat, locLong){
+  //   console.log(locLat);
+  //   console.log(locLong);
+  //   northEastCoord = String(locLat + 0.005) + ',' + String(locLong + 0.005)
+  //   southWestCoord = String(locLat - 0.005) + ',' + String(locLong - 0.005)
+  //   console.log(northEastCoord)
+  //   console.log(southWestCoord)
+  // }
 
-  var clearInput = function(){
-    $('#search-input').click(function(){
-      $('#search-input').val('');
-    })
-  }
+  // var clearInput = function(){
+  //   $('#search-input').click(function(){
+  //     $('#search-input').val('');
+  //   })
+  // }
 
   // GMaps Styles
   var styles = [
-    {"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"administrative.neighborhood","elementType":"labels.text","stylers":[{"visibility":"on"},{"color":"#f35c19"},{"weight":"0.01"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"visibility":"on"},{"color":"#f7e5d9"},{"lightness":"0"}]},{"featureType":"landscape","elementType":"labels.text","stylers":[{"weight":"0.64"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"visibility":"on"},{"weight":"1"},{"lightness":"63"},{"color":"#fff5f0"}]},{"featureType":"poi","elementType":"labels","stylers":[{"color":"#ffad00"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#ffc3a0"},{"lightness":"1"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"visibility":"on"},{"color":"#fd7539"},{"weight":"0.62"},{"lightness":"53"}]},{"featureType":"road.highway","elementType":"labels.text","stylers":[{"visibility":"on"},{"color":"#4e5757"},{"weight":"0.01"}]},{"featureType":"road.highway","elementType":"labels.text.fill","stylers":[{"weight":"0.01"}]},{"featureType":"road.highway","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"},{"visibility":"on"},{"weight":"0.01"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#c6e3ec"},{"visibility":"on"}]},{"featureType":"water","elementType":"geometry","stylers":[{"visibility":"on"},{"color":"#cde1f0"},{"lightness":"-3"}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"weight":"0.25"},{"color":"#888d8d"}]},{"featureType":"water","elementType":"labels.text.stroke","stylers":[{"weight":"0.80"}]
-    }
+    {"featureType":"administrative.country", "elementType":"labels.text.fill", "stylers":[{"color":"#ff5a5f"}]},
+    {"featureType":"administrative.province","elementType":"geometry.stroke","stylers":[{"visibility":"off"}]},
+    {"featureType":"administrative.province","elementType":"labels.text","stylers":[{"visibility":"off"}]},
+    {"featureType":"administrative.locality","elementType":"labels.text","stylers":[{"visibility":"on"},{"saturation":"0"},{"lightness":"0"}]},
+    {"featureType":"administrative.locality","elementType":"labels.text.fill","stylers":[{"lightness":"-15"},{"color":"#ff5a5f"}]},
+    {"featureType":"administrative.locality","elementType":"labels.text.stroke","stylers":[{"lightness":"55"},{"gamma":"0.33"},{"weight":"5.50"},{"color":"#ffffff"},{"saturation":"-13"},{"visibility":"on"}]},
+    {"featureType":"administrative.neighborhood","elementType":"labels.text","stylers":[{"visibility":"on"}]},
+    {"featureType":"administrative.neighborhood","elementType":"labels.text.fill","stylers":[{"color":"#ff5a5f"}]},
+    {"featureType":"administrative.neighborhood","elementType":"labels.text.stroke","stylers":[{"weight":"4.70"},{"lightness":"40"},{"color":"#ffffff"}]},
+    {"featureType":"landscape","elementType":"geometry.fill","stylers":[{"lightness":"38"},{"color":"#f5f6f5"}]},
+    {"featureType":"landscape.man_made","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"lightness":"-2"},{"color":"#ecedec"}]},
+    {"featureType":"poi","elementType":"labels.text.fill","stylers":[{"color":"#ff5a5f"}]},
+    {"featureType":"poi","elementType":"labels.text.stroke","stylers":[{"weight":"5.12"},{"gamma":"1.52"},{"lightness":"100"},{"color":"#ffffff"}]},
+    {"featureType":"poi.business","elementType":"all","stylers":[{"visibility":"off"}]},
+    {"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"color":"#bce4c1"}]},
+    {"featureType":"poi.park","elementType":"labels.icon","stylers":[{"visibility":"on"}]},
+    {"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"lightness":"27"}]},
+    {"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"lightness":"0"},{"weight":"0.26"}]},
+    {"featureType":"road.highway","elementType":"labels","stylers":[{"visibility":"on"},{"saturation":"20"}]},
+    {"featureType":"road.highway","elementType":"labels.icon","stylers":[{"gamma":"1.42"}]},
+    {"featureType":"road.highway.controlled_access","elementType":"geometry.fill","stylers":[{"lightness":"-16"},{"saturation":"67"}]},
+    {"featureType":"road.highway.controlled_access","elementType":"labels.icon","stylers":[{"visibility":"on"}]},
+    {"featureType":"road.arterial","elementType":"geometry.stroke","stylers":[{"lightness":"20"}]},
+    {"featureType":"road.arterial","elementType":"labels.text.fill","stylers":[{"lightness":"28"}]},
+    {"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},
+    {"featureType":"road.local","elementType":"geometry.stroke","stylers":[{"lightness":"28"}]},
+    {"featureType":"transit.station.airport","elementType":"labels.text.fill","stylers":[{"color":"#7b0051"}]},
+    {"featureType":"transit.station.bus","elementType":"labels.text.fill","stylers":[{"color":"#7b0051"}]},
+    {"featureType":"transit.station.rail","elementType":"labels.text.fill","stylers":[{"color":"#7b0051"}]},
+    {"featureType":"water","elementType":"geometry.fill","stylers":[{"saturation":"-30"},{"lightness":"18"},{"color":"#b2f1ec"}]}
   ]
-  
+
   // GMaps Initializer 
   function initialize() {
-    var locationResults = {}
-    console.log('map is initialized');
+    console.log('initialize()');
+
     var geocoder = new google.maps.Geocoder();
-    function codeAddress() {
-      console.log('codeAddress function executed')
-      var address = document.getElementById('search-input').value;
-      geocoder.geocode( { 'address': address}, function(results, status) {
-        console.log(results)
-        locationResults = results
-        if (status == google.maps.GeocoderStatus.OK) {
-          map.setCenter(results[0].geometry.location);
-          var marker = new google.maps.Marker({
-              map: map,
-              position: results[0].geometry.location
-          });
-          markers.push(marker);
-        } else {
-          alert('Goshdarnit! We could not find your coordinates because: ' + status);
-        }
-      });
-    }
-    var centerLocation = new google.maps.LatLng(51.5008, 0.1247) /*London*/
+
+    // var geocodeRequestObj = {
+    //   address: string,
+    //   location: LatLng,
+    //   bounds: LatLngBounds,
+    //   region: 'GB'
+    // }
+    var centerLocation = new google.maps.LatLng(51.5008, -0.1090) /*London*/
     var mapOptions = {
       center: centerLocation,
-      zoom: 11,
+      zoom: 12,
       styles: styles,
-      backgroundColor: "#FFD000",
+      backgroundColor: "#FFFC78",
       disableDefaultUI: true,
       streetViewControl: true
     };
@@ -78,17 +97,43 @@ $(document).ready(function(){
     var input = (document.getElementById('search-input'));
     var autocomplete = new google.maps.places.Autocomplete(input);
     autocomplete.bindTo('bounds', map);
-    var infowindow = new google.maps.InfoWindow();
-    var marker = new google.maps.Marker({
-      map: map,
-      animation: google.maps.Animation.DROP,
-      anchorPoint: new google.maps.Point(0, -29)
-    });
-    markers.push(marker);
-    google.maps.event.addListener(autocomplete, 'place_changed', function() {
-      infowindow.close();
-      marker.setVisible(false);
+    var mainInfowindow = new google.maps.InfoWindow();
+
+    // function searchAddress() {
+    //   console.log('searchAddress()')
+    //   var address = document.getElementById('search-input').value;
+    //   geocoder.geocode({'address': address, 'region': 'GB'}, function(results, status) {
+    //     console.log('geocode results:' + results)
+    //     if (status == google.maps.GeocoderStatus.OK) {
+    //       map.setCenter(results[0].geometry.location);
+    //       var marker = new google.maps.Marker({
+    //           map: map,
+    //           animation: google.maps.Animation.DROP,
+    //           position: results[0].geometry.location,
+    //           title: results[0].geometry.formatted_address
+    //       });
+    //       searchMarkers.push(marker);
+    //       console.log('search markerss: ' + searchMarkers)
+    //     } else {
+    //       alert('Goshdarnit! We could not find your coordinates because: ' + status);
+    //     }
+    //   });
+    // }
+  
+    google.maps.event.addListener(autocomplete, 'place_changed', function(){
+      if (mainMarker) { mainMarker.setVisible(false); }
+      mainInfowindow.close();
+      searchMarkers = [];
       var place = autocomplete.getPlace();
+      console.log(place)
+      mainCoordinates["latCoord"] = place.geometry.location.A
+      mainCoordinates["longCoord"] = place.geometry.location.A
+      console.log(mainCoordinates)
+      mainMarker = new google.maps.Marker({
+        map: map,
+        animation: google.maps.Animation.DROP,
+        title: place.geometry.formatted_address
+      });
       if (!place.geometry) {
         window.alert("Oopsie dasiy! Please make sure you typed something!");
         return;
@@ -99,9 +144,10 @@ $(document).ready(function(){
         map.setCenter(place.geometry.location);
         map.setZoom(17);
       }
-      marker.setPosition(place.geometry.location);
-      marker.setVisible(true);
-
+      mainMarker.setPosition(place.geometry.location);
+      mainMarker.setVisible(true);
+      searchMarkers.push(mainMarker);
+      console.log(searchMarkers)
       var address = '';
       if (place.address_components) {
         address = [
@@ -110,40 +156,38 @@ $(document).ready(function(){
           (place.address_components[2] && place.address_components[2].short_name || '')
         ].join(' ');
       }
+      mainInfowindow.setContent('<div><strong>' + place.name + '</strong><br>' + '<i>' + place.formatted_address + '</i><br>' + address);
+      mainInfowindow.open(map, mainMarker);
 
-      infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
-      infowindow.open(map, marker);
-      clearMarkers();
-      codeAddress();
-      var resultLat = locationResults[0].geometry.location.A
-      var resultLong = locationResults[0].geometry.location.F
-      $.when(codeAddress()).done(getBusStops(resultLat, resultLong), clearInput())
-      // AJAX request to Digitas API
-      $.ajax({
-        url: 'http://digitaslbi-id-test.herokuapp.com/bus-stops?northEast=' + northEastCoord + '&southWest=' + southWestCoord,
-        data: { format: 'json' },
-        dataType: 'jsonp',
-        type: 'GET',
-        success: function(data){
-          busData = data.markers;
-          console.log(busData);
-          $.each(busData, function(index, value){
-            busStopMarkers[value.id] = {
-              lat: value.lat,
-              lng: value.lng
-            }
-            var latlng = new google.maps.LatLng(value.lat,value.lng);
-            var marker = new google.maps.Marker({
-              position: latlng,
-              map: map,
-              title: value.id
-            }); 
-            marker.setVisible(true);
-          })
-        }
-      })
-      .done(function(){
-      })
+      // var resultLat = locationResults[0].geometry.location.A
+      // var resultLong = locationResults[0].geometry.location.F
+      // $.when(searchAddress()).done(getBusStops(resultLat, resultLong), clearInput())
+      // // AJAX request to Digitas API
+      // $.ajax({
+      //   url: 'http://digitaslbi-id-test.herokuapp.com/bus-stops?northEast=' + northEastCoord + '&southWest=' + southWestCoord,
+      //   data: { format: 'json' },
+      //   dataType: 'jsonp',
+      //   type: 'GET',
+      //   success: function(data){
+      //     busData = data.markers;
+      //     console.log(busData);
+      //     $.each(busData, function(index, value){
+      //       busStopMarkers[value.id] = {
+      //         lat: value.lat,
+      //         lng: value.lng
+      //       }
+      //       var latlng = new google.maps.LatLng(value.lat,value.lng);
+      //       var marker = new google.maps.Marker({
+      //         position: latlng,
+      //         map: map,
+      //         title: value.id
+      //       }); 
+      //       marker.setVisible(true);
+      //     })
+      //   }
+      // })
+      // .done(function(){
+      // })
     });
   }
   google.maps.event.addDomListener(window, 'load', initialize);
